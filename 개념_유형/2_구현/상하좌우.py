d18f96d08a9c9f0abd8173c7d72ac1bcafb949ef
mjk0307 <0307mjk@naver.com>
2024-02-05 01:02:58 +0900

    상하좌우 - 반복되는 코드 합치기

--- 개념_유형/2_구현/상하좌우.py
+++ 개념_유형/2_구현/상하좌우.py
@@ -10,33 +10,17 @@ move = list(input().split())
 map = [ [i for i  in range(n)] for _ in range(n)] ## 맵 초기화 
 dx = [1, -1, 0, 0] ## R L D U
 dy = [0, 0, 1, -1] ## R L D U
+move_types = ['R', 'L', 'D', 'U']
 
 x, y = 0, 0 ## 현재 위치 
 for m in move:
-    if m == 'R':
-        x += dx[0]
-        y += dy[0]
-        if (x < 0 or x >= n) or (y < 0 or y >= n): ## 공간 밖은 무시 
-            x -= dx[0]
-            y -= dy[0]
-    elif m == 'L':
-        x += dx[1]
-        y += dy[1]
-        if (x < 0 or x >= n) or (y < 0 or y >= n): ## 공간 밖은 무시 
-            x -= dx[1]
-            y -= dy[1]
-    elif m == 'D':
-        x += dx[2]
-        y += dy[2]
-        if (x < 0 or x >= n) or (y < 0 or y >= n): ## 공간 밖은 무시 
-            x -= dx[2]
-            y -= dy[2]
-    else:
-        x += dx[3]
-        y += dy[3]
-        if (x < 0 or x >= n) or (y < 0 or y >= n): ## 공간 밖은 무시 
-            x -= dx[3]
-            y -= dy[3]
+    idx = move_types.index(m)
+    x += dx[idx]
+    y += dy[idx]
+    if (x < 0 or x >= n) or (y < 0 or y >= n): ## 공간 밖은 무시 
+        x -= dx[idx]
+        y -= dy[idx]
+        
     # print(x, y)
     
 print(x+1, y+1) ## 최종 출력 
