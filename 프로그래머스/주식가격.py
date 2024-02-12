f42fa4c0efd012863a1ece1b9bc86833d4e2c687
mjk0307 <0307mjk@naver.com>
2024-02-12 14:27:46 +0900

    주식 가격

--- 프로그래머스/주식가격.py
+++ 프로그래머스/주식가격.py
@@ -17,7 +17,25 @@ def solution(prices):
             
         times.append(time)
         
-    return times                 
+    return times               
+
+"""
+def solution(prices):
+    times = []
+    
+    for idx in range(len(prices)):
+        time = 0
+        
+        price = prices[idx]
+        for now in prices[idx+1:]:
+            time += 1
+            if price > now:
+                break
+            
+        times.append(time)
+        
+    return times 
+"""
 
 if __name__=='__main__':
     print(solution([1, 2, 3, 2, 3])) ## return 4,3,1,1,0
