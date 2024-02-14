592b9bd77c3da4b88c8b90169355456f9a75d1ba
mjk0307 <0307mjk@naver.com>
2024-02-14 11:02:08 +0900

    n_m 7

---
+++ 백준/N과_M_7.py
@@ -0,0 +1,22 @@
+## N개의 자연수 중에서 M개를 고른 수열
+## 같은 수를 여러 번 골라도 된다. 
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+seq = list(map(int, input().split()))
+seq = sorted(seq) ## 수열은 사전 순으로 증가하는 순서로 출력해야 한다. 
+
+def dfs(sub):
+    
+    if len(sub) == m:
+        print(*sub)
+        return
+    
+    for i in seq:
+        sub.append(i)
+        dfs(sub)
+        sub.pop()
+
+dfs([])
