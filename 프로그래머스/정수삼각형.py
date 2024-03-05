64a7a906bf19650c2d9df91c21c85490afaddbc1
mjk0307 <0307mjk@naver.com>
2024-03-05 20:20:27 +0900

    정수 삼각형 문제 풀이

--- 프로그래머스/정수삼각형.py
+++ 프로그래머스/정수삼각형.py
@@ -1,22 +1,17 @@
 def solution(triangle):
-    dp = triangle[-1][:] ## dp 테이블 초기화 (삼각형의 맨 마지막 줄)
-    print(dp)
     
-    depth = len(triangle) - 1 ## 삼각형의 마지막에서 두번째줄부터 시작
-    
-    for d in reversed(range(depth)):
-        print(f"***{d}***")
-        for idx in range(len(triangle[-1])):
-            print(f"idx: {idx}, num: {triangle[-1][idx]}")
-            print(triangle[d])
-            if idx > 0 and idx <= d:
-                dp[idx] += max(triangle[d][idx-1], triangle[d][idx])
-            elif idx == 0 :
-                dp[idx] += triangle[d][idx]
+    for depth, line in enumerate(triangle):
+        if depth == 0:
+            continue
+        for idx in range(len(line)):
+            if idx > 0 and idx < len(line)-1:
+                line[idx] += max(triangle[depth-1][idx-1], triangle[depth-1][idx])
+            elif idx == 0:
+                line[idx] += triangle[depth-1][idx]
             else:
-                dp[idx] += triangle[d][idx-1]
+                line[idx] += triangle[depth-1][idx-1]
     
-    answer = max(dp)
+    answer = max(triangle[-1])
     return answer
 
 if __name__ =='__main__':
