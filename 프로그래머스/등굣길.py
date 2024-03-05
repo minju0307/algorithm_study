6ee8dfaa24388b6f13ad414ceffd60142ed13502
mjk0307 <0307mjk@naver.com>
2024-03-05 21:06:36 +0900

    완전탐색으로 풀수는 없고 dp를 이용해서 문제 해결

--- 프로그래머스/등굣길.py
+++ 프로그래머스/등굣길.py
@@ -1,26 +1,18 @@
-from collections import deque
-
 def solution(m, n, puddles):
     
-    distances = []
-    dx = [0, 1] ## 가로
-    dy = [1, 0] ## 세로
-    
-    queue = deque([(1,1,0)]) ## (x좌표, y좌표, 이동한 칸의 위치)
-    while queue:
-        x, y, dis = queue.popleft()
+    maps = [[0]*m for _ in range(n)]
+    for _m, _n in puddles:
+        maps[_n-1][_m-1] = -1    
         
-        if x==m and y==n:
-            distances.append(dis)
-        
-        for i in range(2):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            
-            if nx > 0 and nx <= m and ny > 0 and ny <= n and [nx, ny] not in puddles:
-                queue.append((nx, ny, dis+1))
-        
-    answer = distances.count(min(distances))
+    maps[0][0] = 1
+    for i in range(n):
+        for j in range(m):
+            if i > 0 and maps[i][j] != -1 and maps[i-1][j] != -1:
+                maps[i][j] += maps[i-1][j]
+            if j > 0 and maps[i][j] != -1 and maps[i][j-1] != -1:
+                maps[i][j] += maps[i][j-1]
+
+    answer = maps[n-1][m-1]
     return answer % 1000000007
 
 if __name__ == '__main__':
