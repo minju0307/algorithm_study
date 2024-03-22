0f8da488f12e55d85b86eb0175f0e21f781fb859
mjk0307 <0307mjk@naver.com>
2024-03-22 23:08:09 +0900

    50개중 29개 테스트케이스 성공

--- sw_academy/오래된_스마트폰.py
+++ sw_academy/오래된_스마트폰.py
@@ -3,102 +3,88 @@ from collections import deque
 T = int(input())
 operation_dict = {"1":"+", "2":"-", "3":"*", "4":"/"}
 
-answers = []
+## 숫자 터치만으로 만들 수 있는 3자리 이하의 수 만들기
+def make_num(depth, now, can_num):
+    if depth == 3:
+        return 
+    
+    for num in can_num:
+        next_num = now*10+num
+        if next_num <= 999:
+            nums.append(next_num)
+            if dp[next_num] <= depth + 1: ## 0이 앞에 와서 똑같은 숫자에 대한 recursion이 일어나는 것을 방지하기 위하여
+                continue
+            dp[next_num] = depth + 1
+            make_num(depth+1, next_num, can_num)
+            
 
-def calculation(_list):
-    nums = []
-    ops = []
-    num = ''
-    for x in _list:
-        if type(x) == int:
-            num += str(x)
+## 계산하는 함수 (조건들을 반영하기)
+def calculation(n1, n2, operator):
+    if operator == "+":
+        result = n1+n2
+    elif operator == "-":
+        result = n1-n2
+    elif operator == "*":
+        result = n1*n2
+    else:
+        if n2 == 0:
+            return False
         else:
-            nums.append(int(num))
-            ops.append(x)
-            num = ''
-    nums.append(int(num))
-    
-    stack1 = nums[::-1]
-    stack2 = ops[::-1]
-    
-    while stack2:
-        n1 = stack1.pop()
-        n2 = stack1.pop()
-        x = stack2.pop()
-        
-        if x == "+":
-            stack1.append(n1+n2)
-        elif x == "-":
-            stack1.append(n1-n2)
-        elif x == "*":
-            stack1.append(n1*n2)
-        else:
-            if n2 == 0:
-                return False
-            else:
-                stack1.append(n1//n2)
-    
-    return stack1[0]
+            result = n1//n2
+    if result < 0 or result > 999:
+        return False
+    else:
+        return result
 
 
 for test_case in range(1, T+1):
-    tmp_answers = []
     # 입력 받기 
     n, o, m = map(int, input().split())
     can_num = list(map(int, input().split()))
     can_op = input().split()
     target = int(input().rstrip())
     
-    q = []
-    for num in can_num:
-        q.append((1, num, [num])) ## 버튼 누른 횟수, 계산 결과, 계산 과정
-    q = deque(q)
-    # print(q)
+    nums = can_num[:]
+    dp = [21 for _ in range(1000)] ## 계산 횟수를 기록하는 dp 테이블 
+    for now in can_num:
+        dp[now] = 1
+        make_num(1, now, can_num) ## 운영할 수 있는 세자리 수를 모두 만들어놓기 
+    nums = sorted(list(set(nums)))
     
-    ## BFS로 가능한 경우를 모두 찾기 
-    while q:
-        # print("***")
-        # print(q)
-        
-        count, result, history = q.popleft()
-        # print(">>>")
-        # print(count, result, history)
-        
-        ## 종료 조건 
-        if result == target:
-            if ("+" in history) or ("-" in history) or ("*" in history) or ("/" in history):
-                answer = count + 1
-                break
-            else:
-                answer = count
-                break
-        elif count >= m :
-            answer = -1
-            break
-        
-        ## 숫자에 해당하는 것들을 다음 queue에 넣기 (숫자가 3개 이상 연달아 갈 수 없음)
-        for num in can_num:
-            next_result = calculation(history+[num])
-            if next_result and 0 <= next_result <= 999:
-                q.append((count+1, next_result, history+[num]))
-        
-        ## 만약 현재가 숫자라면, 연산자에 해당하는 것들을 다음 queue에 넣기
-        if type(history[-1]) == int:
-            for op in can_op:
-                q.append((count+1, result, history+[operation_dict[op]]))
-                        
-    print(f"#{test_case}", answer)
+    ## 만약 숫자만으로 만들 수 있으면 프린트
+    if dp[target] < 21:
+        print(f"#{test_case}", dp[target])
+        continue
+    
+    ## 그것이 아니라면 BFS로 순회하기 
+    q = deque(nums)
 
-# for idx, answer in enumerate(answers):
-#     print(f"#{idx+1}", answer)
+    while q:
+        now = q.popleft()
+        
+        ## 순회하기 
+        for num in nums:
+            for op in can_op:
+                result = calculation(now, num, operation_dict[op])
+                if result and dp[result] > dp[now] + dp[num] + 1 : ## 문제 조건을 벗어나지 않는 숫자가 나왔다면 
+                    dp[result] = dp[now] + dp[num] + 1 ## operator와 = 
+                    q.append(result)
+    
+    if dp[target] == 21:
+        print(f"#{test_case}", -1)
+        continue
+    else:
+        print(f"#{test_case}", dp[target]+1)
+        continue
+
 
     
 '''
 1
-6 4 5
-0 1 2 3 4 7
-1 2 3 4
-5
+5 3 10
+8 7 1 2 6
+2 4 3
+981
 '''
 
 '''
